e4e216295b82cc3851f9009b6c931b1236b45117
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2023-03-12 11:53:37 +0600

    Update primefact.cpp

--- primefact.cpp
+++ primefact.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 vector <int> vec(int n) {
     vector <int> v;
-    for (int i = 2; i*i <= n; i++) {
-        if (n % i == 0)
-        {
+    for (int i = 2; i*i <= n; i++) 
+    {
+        while(n % i == 0) {
             v.push_back(i);
-            while(n % i == 0) n /= i;
+            n /= i;
         }
     }
     if (n > 1) v.push_back(n);
